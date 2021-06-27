10577ce38a3aa13842d6d0c4137b8dbefb23f308
ksuvers <ksulivigo@gmail.com>
2021-06-27 15:01:42 +0300

    change publicPath in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,16 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 if (isDevelopment) {
-  module.exports = {
-    configureWebpack: {
-      devtool: "source-map"
-    },
-    devServer: {
-      proxy: process.env.VUE_APP_API_URL
-    },
-  };
+    module.exports = {
+        configureWebpack: {
+            devtool: "source-map"
+        },
+        devServer: {
+            proxy: process.env.VUE_APP_API_URL
+        },
+    };
 } else {
-  module.exports = {
-    publicPath: "/vue-football/",
-  };
+    module.exports = {
+        publicPath: "/vue-simbirsoft/",
+    };
 }
-
-
-
-
